9f30bf357152122322f00b831cf8eaa16bd53599
Gagan <gagandeepdingh.saimbi@gmail.com>
2023-02-14 17:28:09 +0530

    New version v2.0 , fixed bug that caused alert to popup before filling HTML tags, optimized code for reset button, center the inputs from players in table cells

--- tictactoe/tictac.js
+++ tictactoe/tictac.js
@@ -1,280 +1,322 @@
+
+function myFunction() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+ }
+ 
 let sum=0
 let win = false
-let c1_1 = document.getElementById("1.1")
-let c1_2 = document.getElementById("1.2")
-let c1_3 = document.getElementById("1.3")
+let c1_1 = document.getElementById("t1_1")
+let c1_2 = document.getElementById("t1_2")
+let c1_3 = document.getElementById("t1_3")
 
-let c2_1 = document.getElementById("2.1")
-let c2_2 = document.getElementById("2.2")
-let c2_3 = document.getElementById("2.3")
+let c2_1 = document.getElementById("t2_1")
+let c2_2 = document.getElementById("t2_2")
+let c2_3 = document.getElementById("t2_3")
 
-let c3_1 = document.getElementById("3.1")
-let c3_2 = document.getElementById("3.2")
-let c3_3 = document.getElementById("3.3")
+let c3_1 = document.getElementById("t3_1")
+let c3_2 = document.getElementById("t3_2")
+let c3_3 = document.getElementById("t3_3")
 
     
 function o(tbl) {
+    sum+=1
     if(win == false){
 
-        
-        if(sum%2==0){
-                tbl.innerHTML="O"
+        if(sum<9){
+            if(sum%2==0){
+                    tbl.innerHTML="X"
+            }
+            else{
+                    tbl.innerText="O"
+            }
+            
+                announce();
+              
+            // announce()
+            
         }
         else{
-                tbl.innerText="X"
-        }
-        
-        sum+=1
-        
-        announce()
+            alert("No one Wins. Try again!")
+    }
     }
 }
 
 function game(){
     console.log("Started Game")
     
-    document.getElementById("1.1").onclick=function() {o(c1_1)}
-    document.getElementById("1.2").onclick=function() {o(c1_2)}
-    document.getElementById("1.3").onclick=function() {o(c1_3)}
+    document.getElementById("t1_1").onclick=function() {o(c1_1)}
+    document.getElementById("t1_2").onclick=function() {o(c1_2)}
+    document.getElementById("t1_3").onclick=function() {o(c1_3)}
 
-    document.getElementById("2.1").onclick=function() {o(c2_1)}
-    document.getElementById("2.2").onclick=function() {o(c2_2)}
-    document.getElementById("2.3").onclick=function() {o(c2_3)}
+    document.getElementById("t2_1").onclick=function() {o(c2_1)}
+    document.getElementById("t2_2").onclick=function() {o(c2_2)}
+    document.getElementById("t2_3").onclick=function() {o(c2_3)}
 
-    document.getElementById("3.1").onclick=function() {o(c3_1)}
-    document.getElementById("3.2").onclick=function() {o(c3_2)}
-    document.getElementById("3.3").onclick=function() {o(c3_3)}  
+    document.getElementById("t3_1").onclick=function() {o(c3_1)}
+    document.getElementById("t3_2").onclick=function() {o(c3_2)}
+    document.getElementById("t3_3").onclick=function() {o(c3_3)}  
 }
+
+var squares = document.querySelectorAll('td')
+function reset(){
+    for (let index = 0; index < squares.length; index++) {
+        squares[index].textContent = ''
+        squares[index].style.backgroundColor  = ""
+    }
+    
+    sum=0
+    win=false
+}
+
 
 function announce(){
     //player-1 check
-    if(document.getElementById(1.1).innerText=='O'
+    
+    if(document.getElementById("t1_1").innerText=='O'
         &&
-        document.getElementById(1.2).innerText=='O'
+        document.getElementById("t1_2").innerText=='O'
         &&
-        document.getElementById(1.3).innerText=='O'
+        document.getElementById("t1_3").innerText=='O'
         ){
-        alert("Player-1 wins")
+        
         c1_1.style.backgroundColor = "green"
         c1_2.style.backgroundColor = "green"
         c1_3.style.backgroundColor = "green"
         win = true
-        
+        setTimeout(() => {
+            alert("Player-1 wins")
+        }, 100)
         
     }
-    else if(document.getElementById(2.1).innerText=='O'
+    else if(document.getElementById("t2_1").innerText=='O'
             &&
-            document.getElementById(2.2).innerText=='O'
+            document.getElementById("t2_2").innerText=='O'
             &&
-            document.getElementById(2.3).innerText=='O'
+            document.getElementById("t2_3").innerText=='O'
             ){
-            alert("Player-1 wins")
+           
         c2_1.style.backgroundColor = "green"
         c2_2.style.backgroundColor = "green"
         c2_3.style.backgroundColor = "green"
         win = true
+        setTimeout(() => {
+            alert("Player-1 wins")
+        }, 100)
            
     }
-    else if(document.getElementById(3.1).innerText=='O'
+    else if(document.getElementById("t3_1").innerText=='O'
             &&
-            document.getElementById(3.2).innerText=='O'
+            document.getElementById("t3_2").innerText=='O'
             &&
-            document.getElementById(3.3).innerText=='O'
+            document.getElementById("t3_3").innerText=='O'
             ){
-            alert("Player-1 wins")
+            
             c3_1.style.backgroundColor = "green"
         c3_2.style.backgroundColor = "green"
         c3_3.style.backgroundColor = "green"
         win = true
-    }
-    else if(document.getElementById(1.1).innerText=='O'
-            &&
-            document.getElementById(2.1).innerText=='O'
-            &&
-            document.getElementById(3.1).innerText=='O'
-            ){
+        setTimeout(() => {
             alert("Player-1 wins")
+        }, 100)
+    }
+    else if(document.getElementById("t1_1").innerText=='O'
+            &&
+            document.getElementById("t2_1").innerText=='O'
+            &&
+            document.getElementById("t3_1").innerText=='O'
+            ){
+            
             c1_1.style.backgroundColor = "green"
         c2_1.style.backgroundColor = "green"
         c3_1.style.backgroundColor = "green"
         win = true
-    }
-    else if(document.getElementById(1.2).innerText=='O'
-            &&
-            document.getElementById(2.2).innerText=='O'
-            &&
-            document.getElementById(3.2).innerText=='O'
-            ){
+        setTimeout(() => {
             alert("Player-1 wins")
+        }, 100)
+    }
+    else if(document.getElementById("t1_2").innerText=='O'
+            &&
+            document.getElementById("t2_2").innerText=='O'
+            &&
+            document.getElementById("t3_2").innerText=='O'
+            ){
+            
             c1_2.style.backgroundColor = "green"
         c2_2.style.backgroundColor = "green"
         c3_2.style.backgroundColor = "green"
         win = true
-    }
-    else if(document.getElementById(1.3).innerText=='O'
-            &&
-            document.getElementById(2.3).innerText=='O'
-            &&
-            document.getElementById(3.3).innerText=='O'
-            ){
+        setTimeout(() => {
             alert("Player-1 wins")
+        }, 100)
+    }
+    else if(document.getElementById("t1_3").innerText=='O'
+            &&
+            document.getElementById("t2_3").innerText=='O'
+            &&
+            document.getElementById("t3_3").innerText=='O'
+            ){
+            
             c1_3.style.backgroundColor = "green"
         c2_3.style.backgroundColor = "green"
         c3_3.style.backgroundColor = "green"
         win = true
-    }
-    else if(document.getElementById(1.1).innerText=='O'
-            &&
-            document.getElementById(2.2).innerText=='O'
-            &&
-            document.getElementById(3.3).innerText=='O'
-            ){
+        setTimeout(() => {
             alert("Player-1 wins")
+        }, 100)
+    }
+    else if(document.getElementById("t1_1").innerText=='O'
+            &&
+            document.getElementById("t2_2").innerText=='O'
+            &&
+            document.getElementById("t3_3").innerText=='O'
+            ){
+           
             c1_1.style.backgroundColor = "green"
         c2_2.style.backgroundColor = "green"
         c3_3.style.backgroundColor = "green"
         win = true
-    }
-    else if(document.getElementById(1.3).innerText=='O'
-            &&
-            document.getElementById(2.2).innerText=='O'
-            &&
-            document.getElementById(3.1).innerText=='O'
-            ){
+        setTimeout(() => {
             alert("Player-1 wins")
+        }, 100)
+    }
+    else if(document.getElementById("t1_3").innerText=='O'
+            &&
+            document.getElementById("t2_2").innerText=='O'
+            &&
+            document.getElementById("t3_1").innerText=='O'
+            ){
+            
             c1_3.style.backgroundColor = "green"
         c2_2.style.backgroundColor = "green"
         c3_1.style.backgroundColor = "green"
         win = true
+        setTimeout(() => {
+            alert("Player-1 wins")
+        }, 100)
     }
 
     //player-2 cehck starts
 
-    else if(document.getElementById(1.1).innerText=='X'
+    else if(document.getElementById("t1_1").innerText=='X'
     &&
-    document.getElementById(1.2).innerText=='X'
+    document.getElementById("t1_2").innerText=='X'
     &&
-    document.getElementById(1.3).innerText=='X'
+    document.getElementById("t1_3").innerText=='X'
     ){
-    alert("Player-2 wins")
+    
     c1_1.style.backgroundColor = "green"
     c1_2.style.backgroundColor = "green"
     c1_3.style.backgroundColor = "green"
     win = true
+    setTimeout(() => {
+        alert("Player-2 wins")
+    }, 100)
     
 }
-else if(document.getElementById(2.1).innerText=='X'
+else if(document.getElementById("t2_1").innerText=='X'
         &&
-        document.getElementById(2.2).innerText=='X'
+        document.getElementById("t2_2").innerText=='X'
         &&
-        document.getElementById(2.3).innerText=='X'
+        document.getElementById("t2_3").innerText=='X'
         ){
-        alert("Player-2 wins")
+        
     c2_1.style.backgroundColor = "green"
     c2_2.style.backgroundColor = "green"
     c2_3.style.backgroundColor = "green"
     win = true
-       
-}
-else if(document.getElementById(3.1).innerText=='X'
-        &&
-        document.getElementById(3.2).innerText=='X'
-        &&
-        document.getElementById(3.3).innerText=='X'
-        ){
+    setTimeout(() => {
         alert("Player-2 wins")
+    }, 100)   
+}
+else if(document.getElementById("t3_1").innerText=='X'
+        &&
+        document.getElementById("t3_2").innerText=='X'
+        &&
+        document.getElementById("t3_3").innerText=='X'
+        ){
+        
         c3_1.style.backgroundColor = "green"
     c3_2.style.backgroundColor = "green"
     c3_3.style.backgroundColor = "green"
     win = true
-}
-else if(document.getElementById(1.1).innerText=='X'
-        &&
-        document.getElementById(2.1).innerText=='X'
-        &&
-        document.getElementById(3.1).innerText=='X'
-        ){
+    setTimeout(() => {
         alert("Player-2 wins")
+    }, 100)
+}
+else if(document.getElementById("t1_1").innerText=='X'
+        &&
+        document.getElementById("t2_1").innerText=='X'
+        &&
+        document.getElementById("t3_1").innerText=='X'
+        ){
+        
         c1_1.style.backgroundColor = "green"
     c2_1.style.backgroundColor = "green"
     c3_1.style.backgroundColor = "green"
     win = true
-}
-else if(document.getElementById(1.2).innerText=='X'
-        &&
-        document.getElementById(2.2).innerText=='X'
-        &&
-        document.getElementById(3.2).innerText=='X'
-        ){
+    setTimeout(() => {
         alert("Player-2 wins")
+    }, 100)
+}
+else if(document.getElementById("t1_2").innerText=='X'
+        &&
+        document.getElementById("t2_2").innerText=='X'
+        &&
+        document.getElementById("t3_2").innerText=='X'
+        ){
+       
         c1_2.style.backgroundColor = "green"
     c2_2.style.backgroundColor = "green"
     c3_2.style.backgroundColor = "green"
     win = true
-}
-else if(document.getElementById(1.3).innerText=='X'
-        &&
-        document.getElementById(2.3).innerText=='X'
-        &&
-        document.getElementById(3.3).innerText=='X'
-        ){
+    setTimeout(() => {
         alert("Player-2 wins")
+    }, 100)
+}
+else if(document.getElementById("t1_3").innerText=='X'
+        &&
+        document.getElementById("t2_3").innerText=='X'
+        &&
+        document.getElementById("t3_3").innerText=='X'
+        ){
+        
         c1_3.style.backgroundColor = "green"
     c2_3.style.backgroundColor = "green"
     c3_3.style.backgroundColor = "green"
     win = true
-}
-else if(document.getElementById(1.1).innerText=='X'
-        &&
-        document.getElementById(2.2).innerText=='X'
-        &&
-        document.getElementById(3.3).innerText=='X'
-        ){
+    setTimeout(() => {
         alert("Player-2 wins")
+    }, 100)
+}
+else if(document.getElementById("t1_1").innerText=='X'
+        &&
+        document.getElementById("t2_2").innerText=='X'
+        &&
+        document.getElementById("t3_3").innerText=='X'
+        ){
+        
         c1_1.style.backgroundColor = "green"
     c2_2.style.backgroundColor = "green"
     c3_3.style.backgroundColor = "green"
     win = true
-}
-else if(document.getElementById(1.3).innerText=='X'
-        &&
-        document.getElementById(2.2).innerText=='X'
-        &&
-        document.getElementById(3.1).innerText=='X'
-        ){
+    setTimeout(() => {
         alert("Player-2 wins")
+    }, 100)
+}
+else if(document.getElementById("t1_3").innerText=='X'
+        &&
+        document.getElementById("t2_2").innerText=='X'
+        &&
+        document.getElementById("t3_1").innerText=='X'
+        ){
+       
         c1_3.style.backgroundColor = "green"
     c2_2.style.backgroundColor = "green"
     c3_1.style.backgroundColor = "green"
     win = true
+    setTimeout(() => {
+        alert("Player-2 wins")
+    }, 100)
 }
-}
-
-function reset(){
-        document.getElementById("1.1").innerHTML = ""
-        document.getElementById("1.2").innerHTML = ""
-        document.getElementById("1.3").innerHTML = ""
-
-        document.getElementById("2.1").innerHTML = ""
-        document.getElementById("2.2").innerHTML = ""
-        document.getElementById("2.3").innerHTML = ""
-
-        document.getElementById("3.1").innerHTML = ""
-        document.getElementById("3.2").innerHTML = ""
-        document.getElementById("3.3").innerHTML = ""
-
-        document.getElementById("1.1").style.backgroundColor  = ""
-        document.getElementById("1.2").style.backgroundColor  = ""
-        document.getElementById("1.3").style.backgroundColor  = ""
-
-        document.getElementById("2.1").style.backgroundColor  = ""
-        document.getElementById("2.2").style.backgroundColor  = ""
-        document.getElementById("2.3").style.backgroundColor  = ""
-
-        document.getElementById("3.1").style.backgroundColor  = ""
-        document.getElementById("3.2").style.backgroundColor  = ""
-        document.getElementById("3.3").style.backgroundColor  = ""
-
-        sum=0
-        win=false
 }
